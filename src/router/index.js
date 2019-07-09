a14f56fc07bca98da20c9c8da0411f0b16594c50
Vito <zh.1122521@163.com>
2019-07-09 21:47:33 +0800

    渲染登录页

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,15 @@
 import Vue from 'vue'
 // 引入vue-router模块
 import VueRouter from 'vue-router'
+import Login from '@/views/login'
 // 注册
 Vue.use(VueRouter)
 // 初始化
 const router = new VueRouter({
   // 配置路由规则
-  routes: []
+  routes: [
+    { name: 'login', path: '/login', component: Login }
+  ]
 })
 // 导出
 export default router
---
+++ src/views/login/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login-container">
+    <el-card class="login-box">
+      <img src="../../assets/images/logo_index.png" alt />
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="less">
+.login-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  /* center / cover---背景图定位/背景图尺寸，必须连写才好使 */
+  background: url("../../assets/images/login_bg.jpg") no-repeat center / cover;
+  .login-box {
+    width: 400px;
+    height: 300px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, 0);
+    }
+  }
+}
+</style>
