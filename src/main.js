d66b654cc9ebb2a6f734ef267ea2596b55274a5c
Vito <zh.1122521@163.com>
2019-07-09 17:50:44 +0800

    配置element-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 导入element-ui
+import ELEMENT from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 注册组件
+Vue.use(ELEMENT)
 Vue.config.productionTip = false
 
 new Vue({
