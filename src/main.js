2717e8aa26434d4b965285a2483b29410035fbea
Vito <zh.1122521@163.com>
2019-07-09 18:21:10 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 // 导入element-ui
 import ELEMENT from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 默认索引支持
+// 在使用vue-cli时，@作为前缀在路径中使用时，代表src目录
+import router from './router'
 // 注册组件
 Vue.use(ELEMENT)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+// 引入vue模块
+import Vue from 'vue'
+// 引入vue-router模块
+import VueRouter from 'vue-router'
+// 注册
+Vue.use(VueRouter)
+// 初始化
+const router = new VueRouter({
+  // 配置路由规则
+  routes: []
+})
+// 导出
+export default router
